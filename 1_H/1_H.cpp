da4bb41862d9cb191789263f24e4d1dbb0f98e84
yakushin-mg <146674197+yakushin-mg@users.noreply.github.com>
2023-11-16 20:53:23 +0300

    Update 1_H.cpp

--- 1_H/1_H.cpp
+++ 1_H/1_H.cpp
@@ -1,51 +1,69 @@
 /*Написать алгоритм быстрой сортировки
-1 <= length_array <= 10^5
+1 <= length <= 10^5
+abs(array[i]) <= 10^9 */
+
+/*Написать алгоритм быстрой сортировки
+1 <= length <= 10^5
 abs(array[i]) <= 10^9 */
 
 #include <iostream>
 #include <vector>
 using std::vector;
-void QuickSort(vector<int>& array, vector<int>& extra, size_t start,
-               size_t end) {
-  if (start + 1 < end) {
-    int pivot = array[(start + end) / 2];
-    size_t index_extra = start;
-    for (size_t i = start; i < end; i++) {
-      if (array[i] < pivot) {
-        extra[index_extra++] = array[i];
-      }
+std::pair<size_t, size_t> Partition(vector<int>& array, size_t start,
+                                    size_t end) {
+  std::pair<size_t, size_t> less_bigger_counts{0, 0};
+  if (start + 1 >= end) {
+    return less_bigger_counts;
+  }
+  vector<int> extra;
+  int pivot = array[(start + end) / 2];
+  size_t less_count = 0;
+  for (size_t i = start; i < end; i++) {
+    if (array[i] < pivot) {
+      extra.push_back(array[i]);
+      less_count++;
     }
-    size_t equal_count = 0;
-    for (size_t i = start; i < end; i++) {
-      if (array[i] == pivot) {
-        extra[index_extra++] = array[i];
-        equal_count += 1;
-      }
+  }
+  for (size_t i = start; i < end; i++) {
+    if (array[i] == pivot) {
+      extra.push_back(array[i]);
     }
-    size_t bigger_count = 0;
-    for (size_t i = start; i < end; i++) {
-      if (array[i] > pivot) {
-        extra[index_extra++] = array[i];
-        bigger_count += 1;
-      }
+  }
+  size_t bigger_count = 0;
+  for (size_t i = start; i < end; i++) {
+    if (array[i] > pivot) {
+      extra.push_back(array[i]);
+      bigger_count++;
     }
-    for (size_t i = start; i < end; i++) {
-      array[i] = extra[i];
-    }
-    QuickSort(array, extra, start, end - equal_count - bigger_count);
-    QuickSort(array, extra, end - bigger_count, end);
+  }
+  for (size_t i = 0; i < extra.size(); i++) {
+    array[start + i] = extra[i];
+  }
+  less_bigger_counts = {less_count, bigger_count};
+  return less_bigger_counts;
+}
+
+void QuickSort(vector<int>& array, size_t start, size_t end) {
+  std::pair<size_t, size_t> less_bigger_counts = Partition(array, start, end);
+  size_t less_count = less_bigger_counts.first;
+  size_t bigger_count = less_bigger_counts.second;
+  if (less_count >= 2) {
+    QuickSort(array, start, start + less_count);
+  }
+  if (bigger_count >= 2) {
+    QuickSort(array, end - bigger_count, end);
   }
 }
+
 int main() {
-  size_t length_array;
-  std::cin >> length_array;
-  vector<int> array(length_array);
-  for (size_t i = 0; i < length_array; i++) {
+  size_t length;
+  std::cin >> length;
+  vector<int> array(length);
+  for (size_t i = 0; i < length; i++) {
     std::cin >> array[i];
   }
-  vector<int> extra(length_array);
-  QuickSort(array, extra, 0, length_array);
-  for (size_t i = 0; i < length_array; i++) {
+  QuickSort(array, 0, length);
+  for (size_t i = 0; i < length; i++) {
     std::cout << array[i] << " ";
   }
 }
