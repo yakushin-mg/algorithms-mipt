64056c588087ef97d6bff3b2976b111ef9b4288d
yakushin-mg <146674197+yakushin-mg@users.noreply.github.com>
2024-04-04 00:07:34 +0300

    Update 7_E.cpp

--- 7_E/7_E.cpp
+++ 7_E/7_E.cpp
@@ -1,32 +1,34 @@
-//Дан неориентированный (быть может несвязный) граф. Требуется найти все мосты в нем.
+/* Дан неориентированный (быть может несвязный) граф. Требуется найти все мосты в нем.
+   1 <= points_number <= 20000
+   1 <= roads_number <= 200000
+*/
 
 #include <iostream>
 #include <map>
 #include <set>
-#include <string>
 #include <vector>
 
 class Graph {
  public:
-  std::set<size_t> answer;
+  std::set<size_t> bridges;
   Graph(size_t points_number, size_t roads_number) {
     graph_.resize(points_number + 1);
-    tin_.resize(points_number + 1);
-    ret_.resize(points_number + 1);
+    timein_.resize(points_number + 1);
+    timeout_.resize(points_number + 1);
     used_.resize(points_number + 1);
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     for (size_t i = 0; i < roads_number; i++) {
-      size_t otkuda;
-      size_t kuda;
-      std::cin >> otkuda >> kuda;
-      graph_[otkuda].push_back(kuda);
-      graph_[kuda].push_back(otkuda);
-      number_kolvo_[{otkuda, kuda}].first = i + 1;
-      number_kolvo_[{otkuda, kuda}].second++;
-      number_kolvo_[{kuda, otkuda}].first = i + 1;
-      number_kolvo_[{kuda, otkuda}].second++;
+      size_t from;
+      size_t destination;
+      std::cin >> from >> destination;
+      graph_[from].push_back(destination);
+      graph_[destination].push_back(from);
+      repeat_number_[{from, destination}].first = i + 1;
+      repeat_number_[{from, destination}].second++;
+      repeat_number_[{destination, from}].first = i + 1;
+      repeat_number_[{destination, from}].second++;
     }
   }
   void FindPoints() {
@@ -39,28 +41,28 @@ class Graph {
 
  private:
   std::vector<std::vector<size_t>> graph_;
-  std::vector<size_t> tin_;
-  std::vector<size_t> ret_;
+  std::vector<size_t> timein_;
+  std::vector<size_t> timeout_;
   std::vector<bool> used_;
-  std::map<std::pair<size_t, size_t>, std::pair<size_t, size_t>> number_kolvo_;
+  std::map<std::pair<size_t, size_t>, std::pair<size_t, size_t>> repeat_number_;
   size_t timer_ = 0;
 
   void DFS(size_t vertex, size_t parent = 0) {
-    tin_[vertex] = timer_++;
-    ret_[vertex] = tin_[vertex];
+    timein_[vertex] = timer_++;
+    timeout_[vertex] = timein_[vertex];
     used_[vertex] = true;
-    for (size_t kuda : graph_[vertex]) {
-      if (kuda == parent) {
+    for (size_t destination : graph_[vertex]) {
+      if (destination == parent) {
         continue;
       }
-      if (used_[kuda]) {
-        ret_[vertex] = std::min(ret_[vertex], tin_[kuda]);
+      if (used_[destination]) {
+        timeout_[vertex] = std::min(timeout_[vertex], timein_[destination]);
       } else {
-        DFS(kuda, vertex);
-        ret_[vertex] = std::min(ret_[vertex], ret_[kuda]);
-        if (ret_[kuda] > tin_[vertex]) {
-          if (number_kolvo_[{vertex, kuda}].second == 1) {
-            answer.insert(number_kolvo_[{vertex, kuda}].first);
+        DFS(destination, vertex);
+        timeout_[vertex] = std::min(timeout_[vertex], timeout_[destination]);
+        if (timeout_[destination] > timein_[vertex]) {
+          if (repeat_number_[{vertex, destination}].second == 1) {
+            bridges.insert(repeat_number_[{vertex, destination}].first);
           }
         }
       }
@@ -74,8 +76,8 @@ int main() {
   std::cin >> points_number >> roads_number;
   Graph graph(points_number, roads_number);
   graph.FindPoints();
-  std::cout << graph.answer.size() << "\n";
-  for (size_t point : graph.answer) {
+  std::cout << graph.bridges.size() << "\n";
+  for (size_t point : graph.bridges) {
     std::cout << point << '\n';
   }
 }
