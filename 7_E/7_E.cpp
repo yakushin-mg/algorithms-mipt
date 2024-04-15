482a99cbe9c8d8d8aefe4f0dd2f983a344a9f6bd
yakushin-mg <146674197+yakushin-mg@users.noreply.github.com>
2024-04-16 00:38:06 +0300

    Вынес методы из класса и добавил оператор >>

--- 7_E/7_E.cpp
+++ 7_E/7_E.cpp
@@ -10,74 +10,86 @@
 
 class Graph {
  public:
-  std::set<size_t> bridges;
-  Graph(size_t points_number, size_t roads_number) {
-    graph_.resize(points_number + 1);
-    timein_.resize(points_number + 1);
-    timeout_.resize(points_number + 1);
-    used_.resize(points_number + 1);
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+  std::vector<std::vector<size_t>> roads;
+  std::map<std::pair<size_t, size_t>, std::pair<size_t, size_t>> repeat_number;
+
+  void operator>>(std::istream& input) {
+    size_t points_number;
+    size_t roads_number;
+    input >> points_number;
+    input >> roads_number;
+    roads.resize(points_number + 1);
     for (size_t i = 0; i < roads_number; i++) {
       size_t from;
       size_t destination;
-      std::cin >> from >> destination;
-      graph_[from].push_back(destination);
-      graph_[destination].push_back(from);
-      repeat_number_[{from, destination}].first = i + 1;
-      repeat_number_[{from, destination}].second++;
-      repeat_number_[{destination, from}].first = i + 1;
-      repeat_number_[{destination, from}].second++;
+      input >> from;
+      input >> destination;
+      roads[from].push_back(destination);
+      roads[destination].push_back(from);
+      repeat_number[{from, destination}].first = i + 1;
+      repeat_number[{from, destination}].second++;
+      repeat_number[{destination, from}].first = i + 1;
+      repeat_number[{destination, from}].second++;
     }
   }
-  void FindPoints() {
-    for (size_t i = 1; i < graph_.size(); i++) {
-      if (!used_[i]) {
-        DFS(i);
-      }
-    }
-  }
+
+  std::set<size_t> bridges;
 
  private:
-  std::vector<std::vector<size_t>> graph_;
-  std::vector<size_t> timein_;
-  std::vector<size_t> timeout_;
-  std::vector<bool> used_;
-  std::map<std::pair<size_t, size_t>, std::pair<size_t, size_t>> repeat_number_;
-  size_t timer_ = 0;
+};
 
-  void DFS(size_t vertex, size_t parent = 0) {
-    timein_[vertex] = timer_++;
-    timeout_[vertex] = timein_[vertex];
-    used_[vertex] = true;
-    for (size_t destination : graph_[vertex]) {
-      if (destination == parent) {
-        continue;
-      }
-      if (used_[destination]) {
-        timeout_[vertex] = std::min(timeout_[vertex], timein_[destination]);
-      } else {
-        DFS(destination, vertex);
-        timeout_[vertex] = std::min(timeout_[vertex], timeout_[destination]);
-        if (timeout_[destination] > timein_[vertex]) {
-          if (repeat_number_[{vertex, destination}].second == 1) {
-            bridges.insert(repeat_number_[{vertex, destination}].first);
-          }
+struct Time {
+  size_t timer = 0;
+  std::vector<size_t> in;
+  std::vector<size_t> out;
+};
+
+void DFS(Graph& graph, size_t vertex, size_t parent, std::set<size_t>& bridges,
+         Time& time, std::vector<bool>& used) {
+  time.in[vertex] = time.timer++;
+  time.out[vertex] = time.in[vertex];
+  used[vertex] = true;
+  for (size_t destination : graph.roads[vertex]) {
+    if (destination == parent) {
+      continue;
+    }
+    if (used[destination]) {
+      time.out[vertex] = std::min(time.out[vertex], time.in[destination]);
+    } else {
+      DFS(graph, destination, vertex, bridges, time, used);
+      time.out[vertex] = std::min(time.out[vertex], time.out[destination]);
+      if (time.out[destination] > time.in[vertex]) {
+        if (graph.repeat_number[{vertex, destination}].second == 1) {
+          bridges.insert(graph.repeat_number[{vertex, destination}].first);
         }
       }
     }
   }
-};
+}
+
+std::set<size_t> FindBridges(Graph& graph) {
+  std::set<size_t> bridges;
+  Time time;
+  time.in.resize(graph.roads.size());
+  time.out.resize(graph.roads.size());
+  std::vector<bool> used(graph.roads.size());
+  for (size_t i = 1; i < graph.roads.size(); i++) {
+    if (!used[i]) {
+      DFS(graph, i, 0, bridges, time, used);
+    }
+  }
+  return bridges;
+}
 
 int main() {
-  size_t points_number;
-  size_t roads_number;
-  std::cin >> points_number >> roads_number;
-  Graph graph(points_number, roads_number);
-  graph.FindPoints();
-  std::cout << graph.bridges.size() << "\n";
-  for (size_t point : graph.bridges) {
-    std::cout << point << '\n';
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  Graph graph;
+  graph >> std::cin;
+  std::set<size_t> bridges = FindBridges(graph);
+  std::cout << bridges.size() << "\n";
+  for (size_t bridge : bridges) {
+    std::cout << bridge << '\n';
   }
 }
