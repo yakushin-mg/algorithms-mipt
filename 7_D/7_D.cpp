5fea0b275aaa7b589853ea976112914b27d6574d
yakushin-mg <146674197+yakushin-mg@users.noreply.github.com>
2024-04-16 00:02:39 +0300

    Вынес функции из класса, а также прописал оператор >> для класса Graph

--- 7_D/7_D.cpp
+++ 7_D/7_D.cpp
@@ -9,75 +9,77 @@
 
 class Graph {
  public:
-  std::vector<size_t> components;
-  size_t number_components;
-  Graph(size_t points_number, size_t roads_number) {
-    number_components = 0;
-    graph_.resize(points_number + 1);
-    reverse_graph_.resize(points_number + 1);
-    components.assign(points_number + 1, 0);
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+  std::vector<std::vector<size_t>> graph;
+  std::vector<std::vector<size_t>> reverse_graph;
+
+  void operator>>(std::istream& input) {
+    size_t points_number;
+    size_t roads_number;
+    input >> points_number;
+    input >> roads_number;
+    graph.resize(points_number + 1);
+    reverse_graph.resize(points_number + 1);
     for (size_t i = 0; i < roads_number; i++) {
       size_t from;
       size_t destination;
-      std::cin >> from >> destination;
-      graph_[from].push_back(destination);
-      reverse_graph_[destination].push_back(from);
-    }
-  }
-
-  void FindComponents() {
-    std::vector<bool> used(graph_.size(), false);
-    std::vector<size_t> sort_by_out;
-    for (size_t i = 1; i < graph_.size(); i++) {
-      if (!used[i]) {
-        SortByOut(i, used, sort_by_out);
-      }
-    }
-    std::reverse(sort_by_out.begin(), sort_by_out.end());
-    for (size_t vertex : sort_by_out) {
-      if (components[vertex] == 0) {
-        MarkComponents(vertex);
-        number_components++;
-      }
-    }
-  }
-
- private:
-  std::vector<std::vector<size_t> > graph_;
-  std::vector<std::vector<size_t> > reverse_graph_;
-
-  void SortByOut(size_t vertex, std::vector<bool>& used,
-                 std::vector<size_t>& sort_by_out) {
-    used[vertex] = true;
-    for (size_t destination : graph_[vertex]) {
-      if (!used[destination]) {
-        SortByOut(destination, used, sort_by_out);
-      }
-    }
-    sort_by_out.push_back(vertex);
-  }
-
-  void MarkComponents(size_t vertex) {
-    components[vertex] = number_components + 1;
-    for (size_t destination : reverse_graph_[vertex]) {
-      if (components[destination] == 0) {
-        MarkComponents(destination);
-      }
+      input >> from;
+      input >> destination;
+      graph[from].push_back(destination);
+      reverse_graph[destination].push_back(from);
     }
   }
 };
 
+void SortByOut(Graph& dag, size_t vertex, std::vector<bool>& used,
+               std::vector<size_t>& sort_by_out) {
+  used[vertex] = true;
+  for (size_t destination : dag.graph[vertex]) {
+    if (!used[destination]) {
+      SortByOut(dag, destination, used, sort_by_out);
+    }
+  }
+  sort_by_out.push_back(vertex);
+}
+
+void MarkComponents(Graph& dag, size_t vertex, size_t& number_components,
+                    std::vector<size_t>& components) {
+  components[vertex] = number_components + 1;
+  for (size_t destination : dag.reverse_graph[vertex]) {
+    if (components[destination] == 0) {
+      MarkComponents(dag, destination, number_components, components);
+    }
+  }
+}
+
+std::pair<size_t, std::vector<size_t>> FindComponents(Graph& dag) {
+  size_t number_components = 0;
+  std::vector<size_t> components(dag.graph.size());
+  std::vector<bool> used(dag.graph.size(), false);
+  std::vector<size_t> sort_by_out(dag.graph.size());
+  for (size_t i = 1; i < dag.graph.size(); i++) {
+    if (!used[i]) {
+      SortByOut(dag, i, used, sort_by_out);
+    }
+  }
+  std::reverse(sort_by_out.begin(), sort_by_out.end());
+  for (size_t vertex : sort_by_out) {
+    if (components[vertex] == 0) {
+      MarkComponents(dag, vertex, number_components, components);
+      number_components++;
+    }
+  }
+  return {number_components, components};
+}
+
 int main() {
-  size_t points_number;
-  size_t roads_number;
-  std::cin >> points_number >> roads_number;
-  Graph graph(points_number, roads_number);
-  graph.FindComponents();
-  std::cout << graph.number_components << "\n";
-  for (size_t i = 1; i < graph.components.size(); i++) {
-    std::cout << graph.components[i] << " ";
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  Graph dag;
+  dag >> std::cin;
+  std::pair<size_t, std::vector<size_t>> components = FindComponents(dag);
+  std::cout << components.first - 1 << "\n";
+  for (size_t i = 1; i < components.second.size(); i++) {
+    std::cout << components.second[i] << " ";
   }
 }
